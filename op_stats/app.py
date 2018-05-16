61c2ccfcc1c090983e662deabb99e316a8809afb
santyfajardo <santiago_fajardo96@hotmail.com>
2018-05-16 07:22:17 -0500

    Submit

--- op_stats/app.py
+++ op_stats/app.py
@@ -1,11 +1,11 @@
+import json 
 from flask import Flask
-
+from op_stats.stats  import Stats
 app = Flask(__name__)
 
 @app.route('/v1/stats/cpu')
 def get_cpu_percent():
-  cpu_percent = 100
- #return {'cpu_percent': cpu_percent}
-  return '{}'.format(cpu_percent)
+  cpu_percent = Stats.get_cpu_percent()
+  return json.dumps({cpu_percent': cpu_percent})
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
